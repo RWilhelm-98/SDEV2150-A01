1e106513f29959a004d9b8eafc7c726dfd6f40eb
Rose Wilhelm <RoseVictoriaWilhelm@gmail.com>
2026-03-10 14:32:40 -0600

    add: custom hook, write to react storage

--- 07 - hooks + lifecycle/examples/01 - hooks/student/src/hooks/useSelectedResource.js
+++ 07 - hooks + lifecycle/examples/01 - hooks/student/src/hooks/useSelectedResource.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 
+const STORAGE_KEY = 'selectedResource';
+
 export function useSelectedResource() {
   const [selectedResource, setSelectedResource] = useState(null);
 
   function updateSelectedResource(resource) {
     setSelectedResource(resource);
+    if (resource === null) {
+      sessionStorage.removeItem(STORAGE_KEY);
+    } else {
+      sessionStorage.setItem(STORAGE_KEY, JSON.stringify(resource));
+    }
   }
 
   return [selectedResource, updateSelectedResource];
